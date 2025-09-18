c27360ebc9f266c64364cd594aa512bd09688498
mkueper <michael@kueper-datentechnik.de>
2025-09-18 21:36:25 +0200

    Verzeichnisse aufgeräumt

--- dist/services/blueskyClient.d.ts
+++
@@ -1,7 +0,0 @@
-export function login(): Promise<void>;
-export function postSkeet(text: any): Promise<any>;
-export function getReactions(postUri: any): Promise<{
-    likes: any;
-    reposts: any;
-}>;
-export function getReplies(postUri: any): Promise<any>;
