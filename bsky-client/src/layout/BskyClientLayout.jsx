15ccc5cf8392ccd2b68ef516f0712b1005aad1a0
mkueper <michael@kueper-datentechnik.de>
2025-10-14 11:39:00 +0200

    ui(bsky-client): make timeline header sticky within scroll container (sticky top-0 with backdrop)

--- bsky-client/src/layout/BskyClientLayout.jsx
+++ bsky-client/src/layout/BskyClientLayout.jsx
@@ -17,10 +17,16 @@ export default function BskyClientLayout ({
         />
       </aside>
       <section className='col-span-12 md:col-span-9 lg:col-span-10'>
-        {headerContent ? (
-          <div className='mb-3 border-b pb-2'>{headerContent}</div>
-        ) : null}
-        <div className='max-h-[70vh] overflow-y-auto pr-2'>{children}</div>
+        <div className='max-h-[70vh] overflow-y-auto pr-2'>
+          {headerContent ? (
+            <div className='sticky top-0 z-10 border-b pb-2 bg-background/90 backdrop-blur supports-[backdrop-filter]:bg-background/70'>
+              {headerContent}
+            </div>
+          ) : null}
+          <div className='pt-2'>
+            {children}
+          </div>
+        </div>
       </section>
     </div>
   )
